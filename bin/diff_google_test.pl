6e8233a0e5ad27c4a96c9c9b338567e2d5d3b0ed
David Bond <dbond@128technology.com>
2016-12-01 16:44:47 -0500

    updated to dnf
    updated to dnf

--- bin/diff_google_test.pl
+++ bin/diff_google_test.pl
@@ -1,9 +1,17 @@
 #!/usr/bin/perl
-if ((do { local $/; <STDIN> }) =~ /Actual: "(?<a>.*)".*\n.*\nWhich is: "(?<e>.*)".*/m) {
-  my $e = $+{e};
-  open my $f, '>', '/tmp/actual';
-  if ((my $aw = $+{a}) =~ s/(.{0,60})/$1\n/g ) { print $f $aw; }
-  open my $f, '>', '/tmp/expected';
-  if ((my $ew = $e) =~ s/(.{0,60})/$1\n/g ) { print $f $ew; }
+#
+# A simple script to extract an diff the text coming from a google test expect
+# output.
+if (
+    (
+        do { local $/; <STDIN> }
+    ) =~ /Actual: "(?<a>.*)".*\n.*\nWhich is: "(?<e>.*)".*/m
+  )
+{
+    my $e = $+{e};
+    open my $f, '>', '/tmp/actual';
+    if ( ( my $aw = $+{a} ) =~ s/(.{0,60})/$1\n/g ) { print $f $aw; }
+    open my $f, '>', '/tmp/expected';
+    if ( ( my $ew = $e ) =~ s/(.{0,60})/$1\n/g ) { print $f $ew; }
 }
 `xxdiff /tmp/actual /tmp/expected`
