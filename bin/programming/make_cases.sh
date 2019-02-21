d7397e007c1b015582bacaad8c0cb47b9dfa2c4e
David Bond <mokon@mokon.net>
2019-02-20 21:05:05 -0500

    lots of cleanup
    lots of cleanup

--- bin/programming/make_cases.sh
+++ bin/programming/make_cases.sh
@@ -2,7 +2,7 @@
 # (C) 2013-2019 David 'Mokon' Bond, All Rights Reserved
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    code="${line,,}"
-    #echo -e "        case KEY_${code^^}:\n            return Key::${code^};"
-    echo -e "${code^}"
+  code="${line,,}"
+  #echo -e "        case KEY_${code^^}:\n            return Key::${code^};"
+  echo -e "${code^}"
 done < infile > outfile
