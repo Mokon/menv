d7f9256d68e09ef8602e95617129f956df3246f1
David Bond <mokon@mokon.net>
2021-02-15 17:23:54 -0500

    remove adobe
    remove adobe

--- pkgs/web/src/index.js
+++ pkgs/web/src/index.js
@@ -1,9 +1,9 @@
 const engine = require('./engine.js');
-const board_game_collection = require('./board_game_collection.js');
-const genealogy = require('./genealogy.js');
-const health = require('./health.js');
-const realestate = require('./realestate.js');
-const plot = require('./plot.js');
+//const board_game_collection = require('./board_game_collection.js');
+//const genealogy = require('./genealogy.js');
+//const health = require('./health.js');
+//const realestate = require('./realestate.js');
+//const plot = require('./plot.js');
 
 var staticPages = [
   "contact",
@@ -20,11 +20,11 @@ var staticDataPages = [
   "gallery",
 ];
 
-genealogy.generateRoutes(engine);
-realestate.generateRoutes(engine);
-plot.generateRoutes(engine);
-health.generateRoutes(engine);
-board_game_collection.generateRoutes(engine);
+//genealogy.generateRoutes(engine);
+//realestate.generateRoutes(engine);
+//plot.generateRoutes(engine);
+//health.generateRoutes(engine);
+//board_game_collection.generateRoutes(engine);
 engine.generateRoutes(staticPages, staticDataPages);
 
 engine.run();
